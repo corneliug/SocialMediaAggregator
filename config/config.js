4957b13b68b8b7968597cad2b656c2abaeda15f7
Corneliu-Marian Gosa <corneliu.gosa@gmail.com>
2015-09-14 15:28:31 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "db": "mongodb://sma:sma1@ds031873.mongolab.com:31873/socialmediaaggregator",
+    "db": "**************************",
     "app": {
         "frequency": 3600,
         "postsLimit": 10,
@@ -8,21 +8,21 @@ module.exports = {
     },
     "apps": {
         "twitter": {
-            "key": "zm1Xhonwx2cy7Uv4TAp7WwsAB",
-            "secret": "7Crzdui213HnKwOyDSg1Fu3qJPMjuZEoIzC2KNl38zxR4dcJ8R"
+            "key": "**************************",
+            "secret": "**************************"
         },
         "facebook": {
-            "key": "1020627834636909",
-            "secret": "f7e86d6fa5fa5e95f91e94c47c3e5b27"
+            "key": "**************************",
+            "secret": "**************************"
         },
         "instagram": {
-            "key": "1da7d5643ac64630b99eba92610c7583",
-            "secret": "ca04749b72d047dfa8e03bca7df98ec9",
+            "key": "**************************",
+            "secret": "**************************",
             "redirectUri": "http://localhost:8080/instagram/authcallback",
             "access_token": ""
         },
         "google": {
-            "key": "AIzaSyA7qCOvn3YgjDqeBuNvOLYqnceVfzfCND0"
+            "key": "**************************"
         }
     },
     "accounts": {
