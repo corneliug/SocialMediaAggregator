a760a41d2aaa38bbf968943dde781ed53ce407e9
Corneliu.Gosa <corneliu.gosa@gmail.com>
2015-10-25 09:41:15 +0200

    Implemented Foursquare aggregator

--- social_media_aggregator/data_extractors/FoursquareAggregator.js
+++ social_media_aggregator/data_extractors/FoursquareAggregator.js
@@ -6,119 +6,43 @@ var express = require('express'),
     _ = require('lodash'),
     fs = require('fs');
 
-var searchCriteria = {};
-
-exports.aggregateData = function(userName, agency) {
+exports.aggregateData = function(user, agency) {
     var $that = this;
 
-    AggregatorController.gatherSearchCriteria(userName, agency, 'foursquare', function(criteria){
-        searchCriteria = criteria;
-        console.log(criteria)
-
-        //$that.ensureAuthenticated(function(isAuthenticated){
-        //    if(isAuthenticated){
-                $that.extractData(userName, agency.name, criteria);
-        //    }
-        //});
+    AggregatorController.runWithTimeout(agency.socrata.frequency, null, function(){
+        $that.extractData(user, agency.name, agency.foursquare['feeds']);
     });
 }
 
-/*exports.ensureAuthenticated = function(callback){
-    if(config.apps.instagram.access_token) {
-        return callback(true);
-    }
-    else {
-        fs.readFile(__dirname + '/../../config/instagram-config.js', 'utf8', function (err, data) {
-            if(err) {
-                console.log(err);
-                return callback(false);
-            }
-            data = JSON.parse(data);
-            config.apps.instagram.access_token = data.access_token;
-            return callback(true);
-        });
-    }
-}
-*/
-
-exports.extractData = function(userName, agencyName, criteria){
+exports.extractData = function(user, agencyName, criteria){
     var urlTasks = [];
     var $that = this;
 
-    criteria.url.forEach(function(query){
+    criteria.forEach(function(query){
         urlTasks.push(function(callback){
-            logger.log('debug', 'Extracting data from Foursquare query %s', query);
-            /*$that.getQueryId(query, function(queryid){
-                if(queryid!=undefined){
-                    $that.getLastPostId(query.type, function(lastPostId){
-                        $that.extractUrlPosts(queryid, lastPostId, function(posts){
-                            $that.savePosts(userName, agencyName, '@' + query, posts, callback);
-                        });
-                    });
-                } else {
-                    callback();
-                }
-            });*/
-            $that.getLastPostDate(query.type, function(lastPostDate){
-                $that.extractUrlPosts(query, lastPostDate, function(posts){
-                    $that.savePosts(userName, agencyName, query.type, posts, callback);
-                });
+
+            $that.extractFsquarePosts(user.loc, query.query, function(posts){
+                $that.savePosts(user.name, agencyName, query.query, posts, callback);
             });
 
         });
     });
-
-    /*criteria.url.forEach(function(tag){
-        urlTasks.push(function(callback){
-            $that.getLastPostId('#' + tag, function(lastPostId){
-                $that.extractTagPosts(tag, lastPostId, function(posts){
-                    if(posts!=undefined){
-                        $that.savePosts(userName, agencyName, '#' + tag, posts, callback);
-                    } else {
-                        callback();
-                    }
-                });
-            });
-        });
-    });*/
 
     async.parallel(urlTasks, function(){
     });
 }
 
-/*
-exports.getQueryId = function(query, callback){
-    request({
-        url: query.url,
-        method: 'GET'
-    }, function(error, response, body) {
-        if(error || !body || !response) {
-            return callback(undefined);
-        }
-        else {
-            body = JSON.parse(body);
-
-            return body.data!=undefined && body.data.length!=0 ? callback(body.data[0].id) : callback(undefined);
-        }
-    });
-}
-*/
-
-exports.getLastPostDate = function(match, callback){
-    Post.find({
-        service: 'foursquare',
-        match: match
-    }).sort({
-        date: -1
-    }).exec(function (err, posts) {
-        return posts.length!=0 ? callback(posts[0].date) : callback(undefined);
-    });
+exports.deletePreviousResults = function(username, match){
+    Post.deleteByUserAndPlatformAndAccount(username, 'foursquare', match);
 }
 
-exports.extractUrlPosts = function(query, lastPostDate, callback){
-    var url = query.url;
-    // @todo: url += lastPostDate!=undefined ? "&$where=datetime > " + lastPostDate.toISOString() : "";
-    //url += "&count=" + config.app.postsLimit;
+exports.extractFsquarePosts = function(location, query, callback){
+    var url = "https://api.foursquare.com/v2/venues/explore?ll=" + location.lat + "," + location.lng + "&query=" + query;
+    url += "&limit=" + config.app.postsLimit;
+    url += "&radius=5000";
+    url += "&venuePhotos=1";
+    url += "&client_id=" + config.apps.foursquare.key + "&client_secret=" + process.env.FOURSQUARE_SECRET;
+    url += "&v=20141020";
 
     request({
         url: url,
@@ -129,46 +53,86 @@ exports.extractUrlPosts = function(query, lastPostDate, callback){
         }
         else {
             body = JSON.parse(body);
+            var posts = undefined;
 
-            return callback(body);
+            if(body!=null && body.response!=null && body.response.groups!=null){
+                _.forEach(body.response.groups, function(group){
+                    if(group.type == 'Recommended Places'){
+                        posts = group.items;
+                    }
+                });
+            }
+
+            return callback(posts);
         }
     });
 }
 
 exports.savePosts = function(userName, agencyName, match, posts, callback){
-    var postsTasks = [];
 
-    /*posts.forEach(function(postInfo){
-        postsTasks.push(function(callback){
-            var post = new Post();
+    if(posts!=undefined && posts.length!=0){
+        var postsTasks = [];
 
-            post.userName = userName;
-            post.agencyName = agencyName;
-            post.id = postInfo.casenumber;
-            post.date = new Date(postInfo.datetime);
-            post.date_extracted = new Date();
-            post.service = 'foursquare';
-            //post.account = postInfo.user.username;
-            post.match = match;
-            //post.image = _.get(postInfo, 'images.low_resolution.url') || _.get(postInfo, 'images.thumbnail.url');
-            post.text = postInfo.description;
-            post.likes = 0;
-            post.loc = {
-                type : "Point",
-                coordinates : [parseFloat(postInfo.location_1.longitude), parseFloat(postInfo.location_1.latitude)],
-                address: postInfo.location_1.human_address
-            }
+        posts.forEach(function(postInfo){
+            postsTasks.push(function(callback){
+                var post = new Post();
 
-            //post.url = postInfo.link;
-            //post.icon = postInfo.user.query_picture;
-            post.save();
+                post.userName = userName;
+                post.agencyName = agencyName;
+                post.id = postInfo.venue!=undefined ? postInfo.venue.id : "";
+                post.date = "";
+                post.date_extracted = new Date();
+                post.service = 'foursquare';
+                post.account = '';
+                post.match = '#' + match;
+
+                if(postInfo.venue!=undefined && postInfo.venue.photos!=undefined && postInfo.venue.photos.groups!=undefined){
+                    var photoGroup = postInfo.venue.photos.groups[0];
+                    photoGroup = photoGroup!=undefined && photoGroup.items!=undefined && photoGroup.items.length!=0
+                        ? photoGroup.items[0]
+                        : undefined;
+
+                    post.image = photoGroup!=undefined && photoGroup.suffix!=undefined && photoGroup.prefix!=0
+                        ? photoGroup.prefix + '300x300' + photoGroup.suffix
+                        : '';
+                } else {
+                    post.image = '';
+                }
+
+
+                post.text = postInfo.venue!=undefined & postInfo.venue.name!=undefined ? postInfo.venue.name : '';
+                post.likes = postInfo.venue!=undefined && postInfo.venue.rating!=undefined ? postInfo.venue.rating : 0;
+
+                if(postInfo.venue!=undefined && postInfo.venue.location!=undefined){
+                    post.loc = {
+                        type : "Point",
+                        coordinates : [parseFloat(postInfo.venue.location.lng), parseFloat(postInfo.venue.location.lat)],
+                        address: postInfo.venue.location.formattedAddress!=null && postInfo.venue.location.formattedAddress.length!=0
+                            ? postInfo.venue.location.formattedAddress[0]
+                            : ''
+                    }
+                } else {
+                    post.loc = {};
+                }
+
+
+                post.url = 'https://foursquare.com/v/' + post.id;
+                post.icon = '';
+
+                post.save();
+
+                callback();
+            });
+        });
+
+        async.parallel(postsTasks, function(){
             callback();
         });
-    });*/
 
+    } else {
 
-
-    async.parallel(postsTasks, function(){
         callback();
-    });
+
+    }
+
 }
